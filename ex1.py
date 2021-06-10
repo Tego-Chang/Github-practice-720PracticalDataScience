3107e13a9d4ca3d553fa0d98c05565996a0bff77
Tego-Chang <lendicewatch@gmail.com>
2021-06-10 17:36:34 +0800

    plot mortality against GDP per capita

---
+++ ex1.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from plotnine import * 
+
+df = pd.read_csv("https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv")
+
+#df.head()
+df_plotted = df.loc[:, ['Mortality rate, infant (per 1,000 live births)', 'GDP per capita (constant 2010 US$)', 'Country Name']]
+df_plotted.head()
+
+(ggplot(df_plotted, aes(x='Mortality rate, infant (per 1,000 live births)', y='GDP per capita (constant 2010 US$)')) +
+        # add scatter points
+        geom_point(alpha = 0.5) +
+        # log-scale the x-axis
+        scale_y_log10() +
+        # change labels
+        labs(title="mortality against GDP per capita",
+             x="Mortality rate, infant (per 1,000 live births)",
+             y="GDP per capita (constant 2010 US$)")
+)
+
+"""
+(ggplot(df_plotted, aes(x='Mortality rate, infant (per 1,000 live births)', y='GDP per capita (constant 2010 US$)', color='Country Name')) +
+        # add scatter points
+        geom_point(alpha = 0.5) +
+        # log-scale the x-axis
+        scale_y_log10() +
+        # change labels
+        labs(title="mortality against GDP per capita",
+             x="Mortality rate, infant (per 1,000 live births)",
+             y="GDP per capita (constant 2010 US$)",
+             color="Country Name")
+)
+"""
+
+"""
+Then pull out the columns:
+
+‘Mortality rate, infant (per 1,000 live births)’
+‘GDP per capita (constant 2010 US$)’
+‘Country Name’
+and plot mortality against GDP per capita.
+"""
+
+
